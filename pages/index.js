cfe24f0d0c48e14c35f12df1535df7f48aac85af
Chris <chrisjobando@gmail.com>
2019-10-26 19:22:22 -0400

    Styling updates

--- pages/index.js
+++ pages/index.js
@@ -49,36 +49,39 @@ class Home extends Component {
     const { items, result } = this.state;
 
     return (
-      <div>
-        <Jumbotron>
-          <h1 className="display-5">Scanning Unit</h1>
-          <p className="lead">Ask customers to scan their order QR's below</p>
-          <p className="lead"> </p>
+      <div style={{ backgroundColor: '#f9f9f9', height: '100vh' }} >
+        <Jumbotron className="header" style={{ backgroundColor: '#54b948' }}>
+          <h1 className="display-5" style={{ fontWeight: '600', fontSize: '54px', color: '#fff' }} >Scanning Unit</h1>
+          <p className="subhead" style={{ fontWeight: '600', fontSize: '28px' }} >Ask Customers to Scan their Order QR Below</p>
         </Jumbotron>
         <Head>
           <title>FindIt</title>
           <link rel='icon' href='/favicon.ico'/>
         </Head>
 
-        <div className='content flexColumn flexCenter'>
+        <div className='content flexColumn flexCenter' style={{ height: '65vh' }}>
           <QrReader
-            className='widthSmall'
+            className='qrReader'
+            style= {{ width: '50vh' }}
             delay={300}
             onError={this.handleError}
             onScan={this.handleScan}
           />
 
         </div>
-        <Modal isOpen={this.state.modal}>
-          <ModalHeader toggle={ this.toggle}>Application</ModalHeader>
-          <ModalBody>
+        <Modal isOpen={this.state.modal} >
+          <ModalHeader style={{ backgroundColor: '#54b948', color: '#fff' }} toggle={ this.toggle}><p className="subhead" style={{ margin: '0 auto', fontWeight: '600', fontSize: '28px' }}>Their Order</p></ModalHeader>
+          <ModalBody style= {{ padding: '20px', backgroundColor: '#1e1d1c' }}>
             {result.map(item=>
                 <div>
                   <ListGroup>
-                    <ListGroupItem>
-                      <ListGroupItemHeading>{item.name}</ListGroupItemHeading>
-                      <ListGroupItemText>
-                        Price: {item.price} Number: {item.quantity}
+                    <ListGroupItem style={{ margin: '15px' }}>
+                      <ListGroupItemHeading style={{ fontWeight: '600', fontSize: '32px', textTransform: 'capitalize' }}>{item.name}</ListGroupItemHeading>
+                      <ListGroupItemText className='listItem'>
+                        Price: ${item.price}
+                      </ListGroupItemText>
+                      <ListGroupItemText className='listItem'>
+                        Quantity: {item.quantity}
                       </ListGroupItemText>
                     </ListGroupItem>
                   </ListGroup>
